a49dda0dacbceb552491907addd698ff6b899eaf
JoseMorelVera <j.morel@duocuc.cl>
2026-07-19 22:10:28 -0400

    Create RutInvalidoException.java

---
+++ RutInvalidoException.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package util;
+
+/**
+ *
+ * @author josem
+ */
+public class RutInvalidoException extends Exception {
+    
+    public RutInvalidoException(String mensaje) {
+        super(mensaje);
+    }
+}
